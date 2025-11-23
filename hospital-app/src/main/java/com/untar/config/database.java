46ebf7d440327718d69ddbb6a5e1848b8aa3a542
Ricky Wijaya <rickyw99.bjr@gmail.com>
2025-11-23 17:16:46 +0700

    Update Database

--- hospital-app/src/main/java/com/untar/config/database.java
+++ hospital-app/src/main/java/com/untar/config/database.java
@@ -1,18 +1,38 @@
 package com.untar.config;
 
 import org.sql2o.Sql2o;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Database {
+
     private static Sql2o sql2o;
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/db_hospital"; 
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "database123"; //Password ricky
 
     private Database() {}
 
     public static Sql2o getSql2o() {
         if (sql2o == null) {
-            sql2o = new Sql2o(DB_URL, DB_USER, DB_PASSWORD);
+            try {
+                Properties prop = new Properties();
+                InputStream input = Database.class.getClassLoader().getResourceAsStream("application.properties");
+
+                if (input == null) {
+                    System.out.println("Maaf, tidak bisa menemukan file application.properties");
+                    return null;
+                }
+
+                prop.load(input);
+
+                String dbUrl = prop.getProperty("db.url");
+                String dbUser = prop.getProperty("db.username");
+                String dbPass = prop.getProperty("db.password");
+
+                sql2o = new Sql2o(dbUrl, dbUser, dbPass);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error saat membaca konfigurasi database: " + e.getMessage());
+            }
         }
         return sql2o;
     }
