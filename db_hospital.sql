178e5836bfbeff2ec0bb8d46b579716d98ec7178
wilchans <wilsen.chandra03@gmail.com>
2025-11-24 12:52:31 +0700

    Add files via upload

--- db_hospital.sql
+++ db_hospital.sql
@@ -1,1 +1,11 @@
-SELECT * FROM users;
+CREATE DATABASE db_hospital;
+
+USE db_hospital;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'user'
+);
